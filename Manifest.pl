e86122561431ed72b1de7f89d7183a57b87ff9f4
Jose F. Morales <jfmcjf@gmail.com>
2018-12-06 12:24:25 +0100

    Updated version numbers
    Src-commit: 6947d8915b61774272279812e42a653bc48117ff

--- Manifest.pl
+++ Manifest.pl
@@ -1,7 +1,7 @@
 :- bundle(synch_actions).
 version('1.0').
 depends([
-    core-[version>='1.16']
+    core-[version>='1.18']
 ]).
 cmd('cmds/synch_actions').
 manual('synch_actions', [main='doc/SETTINGS.pl']).
