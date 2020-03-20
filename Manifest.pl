d685b74f1fa95bb1aefdd432d92d545f5432897f
Jose F. Morales <jfmcjf@gmail.com>
2020-03-20 15:20:26 +0100

    Bump version to 1.19
    Src-commit: e0f5593d0bd46e6cc689955ae1a8014910e16165

--- Manifest.pl
+++ Manifest.pl
@@ -1,7 +1,7 @@
 :- bundle(synch_actions).
 version('1.0').
 depends([
-    core-[version>='1.18']
+    core-[version>='1.19']
 ]).
 cmd('cmds/synch_actions').
 manual('synch_actions', [main='doc/SETTINGS.pl']).
