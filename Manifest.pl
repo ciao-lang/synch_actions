e0067b50003c9ebbbdcfd88afcb845fd78e52eb1
Jose F. Morales <jfmcjf@gmail.com>
2022-09-28 17:42:18 +0200

    Updating version 1.22.0
    Src-commit: 3b5f0895bab4700718bc79deed54fffdc1dcd374

--- Manifest.pl
+++ Manifest.pl
@@ -1,7 +1,7 @@
 :- bundle(synch_actions).
 version('1.0').
 depends([
-    core-[version>='1.21']
+    core-[version>='1.22']
 ]).
 cmd('cmds/synch_actions').
 manual('synch_actions', [main='doc/SETTINGS.pl']).
