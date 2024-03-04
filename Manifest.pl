de463370b51dd53b8dc25f6567beacd5cbf784d6
Jose F. Morales <jfmcjf@gmail.com>
2024-03-04 18:44:59 +0100

    Update core version dependency
    Src-commit: e9ad65b5cac84bf3d5dd3839edda06d67cf78df2

--- Manifest.pl
+++ Manifest.pl
@@ -1,7 +1,7 @@
 :- bundle(synch_actions).
 version('1.0').
 depends([
-    core-[version>='1.22']
+    core-[version>='1.23']
 ]).
 cmd('cmds/synch_actions').
 manual('synch_actions', [main='doc/SETTINGS.pl']).
